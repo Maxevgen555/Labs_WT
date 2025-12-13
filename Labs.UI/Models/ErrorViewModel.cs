03488f6509b0d7844b3a51e283e4909c2736b24f
Maxevgen555 <maxevgen555@gmail.com>
2025-12-13 22:26:07 +0300

    Добавьте файлы проекта.

---
+++ Labs.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Labs.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
