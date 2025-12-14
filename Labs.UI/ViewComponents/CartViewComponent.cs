5f9eb72699c82622cd8dc80ea8d21fe92575825f
Maxevgen555 <maxevgen555@gmail.com>
2025-12-14 15:00:00 +0300

    Лаб. работа №2

---
+++ Labs.UI/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Labs.UI.ViewComponents
+{
+    public class CartViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
